f9245025b43a892a4d0087868b8bbe50563644d2
parkcheung <1025711995@qq.com>
2018-03-04 21:53:49 +0800

    code commint

--- wechat/get_article_list.py
+++ wechat/get_article_list.py
@@ -20,7 +20,7 @@ head = {
     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
-    'Cookie': 'rewardsn=; wxtokenkey=52363034789e417a147b34ee1592791c2faf0dbc3e17eab8c5c23f5d2fccdab5; wxuin=338385195; devicetype=Windows10; version=62060028; lang=zh_CN; pass_ticket=BzTu+TqcSTidBngcCpl/I1MyUnjkvwlJ9RXDEBD1b+k2ijuomwg/kE/s2Y+QUO5e; wap_sid2=CKuyraEBElx6eDdRQzBrTEVwQ2dpdGI2dFNQMXJ4X185WXMtTnB5a2NTenVtdmJsaHZ3d2ZzOFozaU9XdjJsOWJmNGJTcUNvbFlLVzVDNjdWWXd6N3Y5UnEtclZVYXNEQUFBfjCSivbSBTgNQJVO',
+    'Cookie': 'wxuin=338385195; devicetype=Windows10; version=62060028; lang=zh_CN; pass_ticket=9v1YpDhPxTO7rsg7FARokFZmMqF8c9H6TSNNoRwwN03iXEir2F9yy6OzIRAGSKP3; wap_sid2=CKuyraEBElxNbktXRG5ETW41YlMyanhSd0RWM2ZnNmhkMVlxQl9OVUtsQUhoS0xqZloyb1FlaWNjeVFCMnZiTXdPOGtPLXpOMVRIaHk0V2hRalVuZWRGQlV2TGhhSzBEQUFBfjCFv6fTBTgNQJVO',
     'Pragma': 'no-cache',
     'Referer': 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MjM5NjA4NzUwNw==&scene=123&uin=MzM4Mzg1MTk1&key=a5093a3f4494c6b19f6e56165af5e5625076f5a6ac48e36474027d512210395610ea2b2e65ec43c246d905fba8054e628cd34e23819baa2b177e818db6d9a3bf4d0f791b67f964bc17d47e4763ec30e4&devicetype=Windows+10&version=62060028&lang=zh_CN&a8scene=1&pass_ticket=BzTu%2BTqcSTidBngcCpl%2FI1MyUnjkvwlJ9RXDEBD1b%2Bk2ijuomwg%2FkE%2Fs2Y%2BQUO5e&winzoom=1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat QBCore/3.43.691.400 QQBrowser/9.0.2524.400',
@@ -32,8 +32,7 @@ head = {
 def get_article_list(page):
     page = page - 1
     offset = page * 10
-    user_url = "https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz=MjM5NjA4NzUwNw==&f=json&offset=" + str(
-        offset) + "&count=10&is_ok=1&scene=123&uin=MzM4Mzg1MTk1&key=a5093a3f4494c6b19f6e56165af5e5625076f5a6ac48e36474027d512210395610ea2b2e65ec43c246d905fba8054e628cd34e23819baa2b177e818db6d9a3bf4d0f791b67f964bc17d47e4763ec30e4&pass_ticket=BzTu%2BTqcSTidBngcCpl%2FI1MyUnjkvwlJ9RXDEBD1b%2Bk2ijuomwg%2FkE%2Fs2Y%2BQUO5e&wxtoken&appmsg_token=939_fREbmAFlNVsYzA%252BM6-8jmuHDXZ3WNVj73VfTiA~~&x5=0&f=json"
+    user_url = "https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz=MjM5NjA4NzUwNw==&f=json&offset="+str(page)+"0&count=10&is_ok=1&scene=123&uin=MzM4Mzg1MTk1&key=3ba6ee9bdf552fca69f935e01c19a944e2cc9af2c261a29fb4550f43c15eb68aff309fedd01612023848867a45c38555dd0b2f9be05be728fbc5330a7ba23854744614a9fb1f875a2aad716a2b493e28&pass_ticket=9v1YpDhPxTO7rsg7FARokFZmMqF8c9H6TSNNoRwwN03iXEir2F9yy6OzIRAGSKP3&wxtoken=&appmsg_token=941_uz0eItIp0nkxwFUIPVZDeWRNtVaN1BDd9jRKJg~~&x5=0&f=json"
 
     r = requests.get(user_url, headers=head)
     articles_list = json.loads(r.json()["general_msg_list"])["list"]
